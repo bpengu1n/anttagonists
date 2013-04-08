0bb1876b984148666cb82bdd62cbd3da892b5df9
Benjamin Yunker <bwyunker@gmail.com>
2013-04-08 16:09:48 -0600

    Update Field.java

--- src/antsimulation/model/Field.java
+++ src/antsimulation/model/Field.java
@@ -9,7 +9,10 @@ public class Field {
     private Colony[] colonies;
     private double[][][] pheromones;    //pheromones[faction][x][y]
     
-    public void initialize() {}
+    public void initialize() {
+        // Initialize ants, predators, foodpiles, and colonies, randomized by width, height
+        // Set parameters, e.g. ants time to die,
+    }
     public void spawnPredator() {}
     public double getPheromoneAt(int faction, int x, int y) { return 1; }
     public void setPheromoneAt(int faction, int x, int y, double value) { pheromones[faction][x][y] = value; }
