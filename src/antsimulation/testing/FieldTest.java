4d294012c4f876dbe4f1fb70817fe12ce35b0f67
Benjamin Yunker <bwyunker@gmail.com>
2013-04-10 23:10:46 -0600

    Addition of final FieldTest, and mod of Field class
    Modified Field class to allow proper test case, and added FieldTest

--- src/antsimulation/testing/FieldTest.java
+++ src/antsimulation/testing/FieldTest.java
@@ -1,61 +1,86 @@
-package antsimulation.testing;
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package antsimulation.model;
 
-import static org.junit.Assert.*;
-import java.util.Random;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
-
+import static org.junit.Assert.*;
 import antsimulation.ParameterSet;
-import antsimulation.model.*;
 
+/**
+ *
+ * @author bwyunker
+ */
 public class FieldTest {
-  
-	private static int RUN_COUNT = 100;
+    
+    public FieldTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
 
-	@Test
-	public void ConstructorTest() 
-	{
-		Random r = new Random();
-		Foodpile foodpileTest;
-		
-		for(int testRun = 0; testRun < RUN_COUNT; testRun++)
-		{
-			int arg1 = r.nextInt();
-			int arg2 = r.nextInt();
-			int arg3 = r.nextInt();
-			Field arg4 = new Field(new ParameterSet());
-			
-			foodpileTest = new Foodpile(arg1, arg2, arg3, arg4);
-			
-			if(foodpileTest.xLoc!=arg1)
-				fail("FoodPile.xLoc is not properly instanciated.");
-			if(foodpileTest.yLoc!=arg2)
-				fail("FoodPile.yLoc is not properly instanciated.");
-			if(foodpileTest.foodCount!=arg3)
-				fail("FoodPile.foodCount is not properly instanciated.");
-			if(!foodpileTest.field.equals(arg4))
-				fail("FoodPile.field is not properly instanciated.");
-		}		
-	}
-	
-	@Test
-	public void DecrementTest()
-	{
-		Random r = new Random();
-		Foodpile foodpileTest;
-		int foodCount;
-		
-		for(int testRun = 0; testRun < RUN_COUNT; testRun++)
-		{
-			
-			foodCount = r.nextInt();
-			
-			foodpileTest = new Foodpile(0,0,foodCount,null);
-			foodpileTest.decrement();
-			
-			if(foodpileTest.foodCount != (foodCount-1))
-				fail("FoodPile.foodCount is not properly decremented.");
-		}
-		
-	}
+    /**
+     * Test of initialize method, of class Field.
+     */
+    @Test
+    public void testInitialize() {
+        ParameterSet p = new ParameterSet();
+        System.out.println("initialize");
+        Field instance = new Field(p);
+        instance.initialize();
+
+        if (instance.ants.size() < p.checkParameter("StartAntsPerColony")
+                || instance.colonies.size() < p.checkParameter("MaxColonies") 
+                || instance.foodpiles.size() < p.checkParameter("StartFoodPiles")
+                || instance.predators.size() < p.checkParameter("StartPredators"))
+            fail("The Initialization test case failed.");
+    }
+
+    /**
+     * Test of update method, of class Field.
+     */
+    @Test
+    public void testUpdate() {
+        ParameterSet p = new ParameterSet();
+        System.out.println("update");
+        Field instance = new Field(p);
+        for (int x=0; x<500; x++)
+            instance.update();
+        if(instance.changed != true)
+            fail("The Update test case failed.");
+    }
+
+    /**
+     * Test of spawnPredator method, of class Field.
+     */
+    @Test
+    public void testSpawnPredator() {
+        ParameterSet p = new ParameterSet();
+        System.out.println("spawnPredator");
+        Field instance = new Field(p);
+        
+        instance.spawnPredator();
+
+        if (instance.predators.size() < p.checkParameter("StartPredators")) 
+            fail("The Spawn Predator test case failed.");
+    }
 
 }
